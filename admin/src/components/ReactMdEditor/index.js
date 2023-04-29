78b9673aae6651c0c3562a507384422debc9f806
Leo Cheng <lao4chao8@qq.com>
2023-04-29 04:12:33 +0000

    Added katex support

--- admin/src/components/ReactMdEditor/index.js
+++ admin/src/components/ReactMdEditor/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import MDEditor, { commands } from "@uiw/react-md-editor";
+import { getCodeString } from 'rehype-rewrite';
+import katex from 'katex';
+import 'katex/dist/katex.css';
 import MediaLib from "../MediaLib";
 import styled from "styled-components";
 import "@uiw/react-markdown-preview/dist/markdown.css";
@@ -148,6 +151,34 @@ const Editor = ({
           onChange={(newValue) => {
             onChange({ target: { name, value: newValue || "" } });
           }}
+          previewOptions={{
+            components: {
+              code: ({ inline, children = [], className, ...props }) => {
+                const txt = children[0] || '';
+                if (inline) {
+                  if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
+                    const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
+                      throwOnError: false,
+                    });
+                    return <code dangerouslySetInnerHTML={{ __html: html }} />;
+                  }
+                  return <code>{txt}</code>;
+                }
+                const code = props.node && props.node.children ? getCodeString(props.node.children) : txt;
+                if (
+                  typeof code === 'string' &&
+                  typeof className === 'string' &&
+                  /^language-katex/.test(className.toLocaleLowerCase())
+                ) {
+                  const html = katex.renderToString(code, {
+                    throwOnError: false,
+                  });
+                  return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />;
+                }
+                return <code className={String(className)}>{txt}</code>;
+              },
+            },
+          }}          
         />
         <div style={{ padding: "50px 0 0 0" }} />
         <MediaLib
